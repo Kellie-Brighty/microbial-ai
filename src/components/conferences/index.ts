38c0ef92e8d5bc5534b20a43ee33fb3cdc7a4ff4
Kelly Owoju <kelliebrighty@gmail.com>
2025-05-01 08:53:25 +0100

    Fixed some issues

--- src/components/conferences/index.ts
+++ src/components/conferences/index.ts
@@ -2,3 +2,5 @@
 export { default as ConferenceCard } from "./ConferenceCard";
 export { default as ConferencePlayer } from "./ConferencePlayer";
 export { default as LiveConferenceSection } from "./LiveConferenceSection";
+
+
